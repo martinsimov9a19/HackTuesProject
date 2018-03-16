d698558ff64994944994f06fb85560a233b68786
martinsimov9a19 <martinsimov519@gmail.com>
2018-03-16 15:55:41 +0200

    Added Player Control and Camera Movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform playerCam, character, centerPoint;
+
+    private float mouseX, mouseY;
+    public float mouseSensitivity = 10f;
+    public float mouseYPosition = 1f;
+
+    private float zoom;
+    public float zoomSpeed = 2;
+
+    public float zoomMin = -2f;
+    public float zoomMax = -10f;
+
+    public float rotationSpeed = 5f;
+    // Use this for initialization
+    void Start () {
+        zoom = -3;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        zoom += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
+        if (zoom > zoomMin)
+            zoom = zoomMin;
+
+        if (zoom < zoomMax)
+            zoom = zoomMax;
+
+        playerCam.transform.localPosition = new Vector3(0, 0, zoom);
+
+        if (Input.GetMouseButton(1))
+        {
+            mouseY -= Input.GetAxis("Mouse Y");
+        }
+
+        mouseY = Mathf.Clamp(mouseY, -60f, 60f);
+        playerCam.LookAt(centerPoint);
+        centerPoint.localRotation = Quaternion.Euler(mouseY, mouseX, 0);
+    }
+}
