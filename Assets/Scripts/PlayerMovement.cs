5831d16def3b98aaa5ecc784f8d39828fda9b0bd
martinsimov9a19 <martinsimov519gmail.com>
2018-03-18 00:42:57 +0200

    Added Crashed Airplane and Ship to terrain. Major code changes.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,8 +14,10 @@ public class PlayerMovement : MonoBehaviour {
     CharacterController controller;
 
     public Canvas GUICanvas;
-
+    public Button yourButton;
     public Text question;
+
+
 
 
     int randomIndexAnswer;
@@ -26,12 +28,14 @@ public class PlayerMovement : MonoBehaviour {
     List<int> Used;
     public Button[] buttons;
     public Text[] texts;
+    public Transform[] Lilypads;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         controller = GetComponent<CharacterController>();
         Used = new List<int>();
+        Transform lilyPad = Lilypads[i].GetComponent<Transform>();
     }
 
     void Update()
@@ -57,12 +61,24 @@ public class PlayerMovement : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.tag == "Water")
+        {
+            Debug.Log("You Have bean eaten by a shark!");
+            transform.position = new Vector3(238,52,240);
+
+        }
+        if (collision.gameObject.tag == "Ship")
+        {
+            Debug.Log("THE END");
+            controller.enabled = false;
+            rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+        }
         if (collision.gameObject.tag == "LillyPad")
         {
             controller.enabled = false;
             rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
             GUICanvas.enabled = true;
-            int randomIndex = Random.Range(0, 7);
+            int randomIndex = Random.Range(0, 6);
             question.text = Questions[randomIndex];
             Used.Clear();
 
@@ -70,24 +86,53 @@ public class PlayerMovement : MonoBehaviour {
             {
                 randomIndexAnswer = Random.Range(0, 4);
 
-                while(Used.Contains(randomIndexAnswer))
+                while (Used.Contains(randomIndexAnswer))
                 {
                     randomIndexAnswer = Random.Range(0, 4);
                 }
+                
 
+
+                if (randomIndexAnswer == 0)
+                {
+                    Button button = buttons[i].GetComponent<Button>();
+                    buttons[i].onClick.AddListener(TaskOnClickCorrect);
+              
+                }
 
                 Used.Add(randomIndexAnswer);
 
                 randomIndexAnswer.ToString();
                 texts[i].text = Answers[i, randomIndexAnswer];
-
-                Button ButtonAnswer = buttons[i].GetComponent<Button>();  
+                if (randomIndexAnswer != 0)
+                {
+                    Button button = buttons[i].GetComponent<Button>();
+                    button.onClick.AddListener(TaskOnClickWrong);
+                }
             }
-
-
-
-
-            
         }
+    }
+
+    int i = 1;
+
+    void TaskOnClickWrong()
+    {
+        Debug.Log("Wrong!");
+    }
+
+    void TaskOnClickCorrect()
+    {
+        Debug.Log("Correct!");
+        rb.constraints &= RigidbodyConstraints.None;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
+        GUICanvas.enabled = false;
+        controller.enabled = true;
+        transform.position = Lilypads[i].position;
+
+        if (i < 9)
+        {
+            i++;
+        }
+        
     }
 }
